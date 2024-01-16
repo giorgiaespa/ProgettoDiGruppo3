6768d81d77187cbe7ffd315525f0c96b7333690d
gio <145365018+giorgiaespa@users.noreply.github.com>
2024-01-16 23:35:16 +0100

    Delete file

--- java/src/Amministratore.java
+++
@@ -1,6 +0,0 @@
-public class Amministratore extends Utente {
-
-    public Amministratore(String nome, String cognome, String email, String password) {
-        super(nome, cognome, email, password);
-    }
-}
