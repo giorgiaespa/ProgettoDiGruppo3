71e09086223bdd605ee9e4e39e073abf0372d438
Paola931 <145440087+Paola931@users.noreply.github.com>
2024-01-16 23:51:18 +0100

    Classe Amministratore

---
+++ java/src/Amministratore.java
@@ -0,0 +1,6 @@
+public class Amministratore extends Utente {
+
+    public Amministratore(String nome, String cognome, String email, String password) {
+        super(nome, cognome, email, password);
+    }
+}
